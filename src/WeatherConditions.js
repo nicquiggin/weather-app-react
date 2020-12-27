c081f87d6fbaf81553cce76c3c838ed0230b096f
nicquiggin <71301277+nicquiggin@users.noreply.github.com>
2020-12-27 11:59:37 -0700

    Integrated API into weather conditions component

--- src/WeatherConditions.js
+++ src/WeatherConditions.js
@@ -1,28 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./WeatherConditions.css";
 
 export default function WeatherConditions() {
-  let weatherData = {
-    city: "New York",
-    temperature: -5,
-    description: "Cold and sunny",
-    feelsLikeTemp: -8,
-    humidity: 60,
-    wind: 3,
+  let [weatherData, setWeatherData] = useState({loaded: false})
+
+  function handleResponse(response) {
+    console.log(response);
+    setWeatherData({
+    loaded: true,
+    city: (response.data.name),
+    temperature: (response.data.main.temp),
+    description: (response.data.weather[0].description),
+    feelsLikeTemp: (response.data.main.feels_like),
+    humidity: (response.data.main.humidity),
+    wind: (response.data.wind.speed),
+    icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
     unit: "°C"
-  };
-  return (
+    })
+
+  }
+  if (weatherData.loaded) {
+    return (
     <div className="weather-conditions">
       <h1>{weatherData.city}</h1>
 
       <div className="row">
         <div className="col-3 today-weather-icon">
-          <i className="far fa-sun today-weather-icon"></i>
+          <img src={weatherData.icon} alt={weatherData.description} />
         </div>
         <div className="col-5 today-weather-basic">
           <ul>
             <li>
-              {weatherData.temperature}
+              {Math.round(weatherData.temperature)}
               {weatherData.unit}
             </li>
             <li>{weatherData.description}</li>
@@ -31,14 +41,23 @@ export default function WeatherConditions() {
         <div className="col-4 today-weather-conditions">
           <ul>
             <li>
-              Feels Like: {weatherData.feelsLikeTemp}
+              Feels Like: {Math.round(weatherData.feelsLikeTemp)}
               {weatherData.unit}
             </li>
             <li>Humidity: {weatherData.humidity}%</li>
-            <li>Wind: {weatherData.wind}km/h</li>
+            <li>Wind: {Math.round(weatherData.wind)}km/h</li>
           </ul>
         </div>
       </div>
     </div>
-  );
+  )
+  } else {
+    let city="Freetown";
+    let apiKey="79432bad8a08e24accbb2ab649dcc7be";
+    let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+
+    axios.get(apiUrl).then(handleResponse)
+    return "Loading"
+  }
+    
 }
